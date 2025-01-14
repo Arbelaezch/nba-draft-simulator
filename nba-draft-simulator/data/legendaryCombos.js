5355b65a43b460fb6aaaca1cd6aefb465c8f465d
Christian Arbelaez <arbelaezch@gmail.com>
2025-01-14 14:28:09 -0700

    LegendaryCombos wording fixed

--- nba-draft-simulator/data/legendaryCombos.js
+++ nba-draft-simulator/data/legendaryCombos.js
@@ -2,67 +2,67 @@ export const LEGENDARY_DUOS = [
     {
         players: ["Kobe Bryant", "Shaquille O'Neal"],
         bonus: 40,
-        description: "One of the most dominant inside-out combinations in NBA history"
+        description: "one of the most dominant inside-out combinations in NBA history"
     },
     {
         players: ["Michael Jordan", "Scottie Pippen"],
         bonus: 45,
-        description: "The greatest wing duo ever, perfect defensive and offensive complement"
+        description: "the greatest wing duo ever. A perfect defensive and offensive complement"
     },
     {
         players: ["Magic Johnson", "Kareem Abdul-Jabbar"],
         bonus: 40,
-        description: "The Showtime Lakers' cornerstone duo"
+        description: "the Showtime Lakers' cornerstone duo"
     },
     {
         players: ["John Stockton", "Karl Malone"],
         bonus: 35,
-        description: "The pick-and-roll masters"
+        description: "the pick-and-roll masters"
     },
     {
         players: ["Stephen Curry", "Klay Thompson"],
         bonus: 35,
-        description: "The Splash Brothers"
+        description: "the Splash Brothers"
     },
     {
         players: ["Kevin Durant", "Stephen Curry"],
         bonus: 30,
-        description: "Unstoppable offensive duo"
+        description: "an unstoppable offensive duo"
     },
     {
         players: ["Tim Duncan", "Tony Parker"],
         bonus: 25,
-        description: "Spurs' dynamic inside-out combo"
+        description: "the Spurs' dynamic inside-out combo"
     },
     {
         players: ["Tim Duncan", "Manu Ginobili"],
         bonus: 25,
-        description: "Spurs' fundamental excellence"
+        description: "perfect examples of the Spurs' fundamental excellence"
     },
     {
         players: ["Larry Bird", "Kevin McHale"],
         bonus: 30,
-        description: "Celtics' legendary frontcourt duo"
+        description: "the Celtics' legendary frontcourt duo"
     },
     {
         players: ["Steve Nash", "Amar'e Stoudemire"],
         bonus: 25,
-        description: "Pick-and-roll perfection"
+        description: "pick-and-roll perfection"
     },
     {
         players: ["Gary Payton", "Shawn Kemp"],
         bonus: 25,
-        description: "The Sonic Boom"
+        description: "the Sonic Boom"
     },
     {
         players: ["Dwyane Wade", "LeBron James"],
         bonus: 35,
-        description: "Dynamic scoring and athletic duo"
+        description: "a dynamic scoring and athletic duo"
     },
     {
         players: ["Kevin Garnett", "Paul Pierce"],
         bonus: 30,
-        description: "Celtics' modern big-wing combo"
+        description: "the Celtics' modern big-wing combo"
     }
 ];
 
@@ -70,27 +70,27 @@ export const LEGENDARY_TRIOS = [
     {
         players: ["Michael Jordan", "Scottie Pippen", "Dennis Rodman"],
         bonus: 60,
-        description: "The Bulls' second three-peat core"
+        description: "the Bulls' second three-peat core"
     },
     {
         players: ["Tim Duncan", "Tony Parker", "Manu Ginobili"],
         bonus: 55,
-        description: "The Spurs' Big Three"
+        description: "the Spurs' Big Three"
     },
     {
         players: ["Larry Bird", "Kevin McHale", "Robert Parish"],
         bonus: 50,
-        description: "Celtics' legendary frontcourt"
+        description: "the Celtics' legendary frontcourt"
     },
     {
         players: ["Magic Johnson", "Kareem Abdul-Jabbar", "James Worthy"],
         bonus: 55,
-        description: "Showtime Lakers' core"
+        description: "the showtime Lakers' core"
     },
     {
         players: ["Stephen Curry", "Klay Thompson", "Draymond Green"],
         bonus: 50,
-        description: "Warriors' championship core"
+        description: "the Warriors' championship core"
     },
     {
         players: ["LeBron James", "Dwyane Wade", "Chris Bosh"],
